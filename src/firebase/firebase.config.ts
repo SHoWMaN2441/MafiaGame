42b6f399d45942c3b9bb7b28ef193ba8a27fcea6
Azizbek <husanovazizbek736@gmail.com>
2026-02-06 08:46:58 +0500

    new datastore

--- src/firebase/firebase.config.ts
+++ src/firebase/firebase.config.ts
@@ -3,13 +3,14 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD5yrJBJKWlmWJ9MDfgk4ZNcbMacGjv6pc",
-  authDomain: "mafiagame-e348d.firebaseapp.com",
-  projectId: "mafiagame-e348d",
-  storageBucket: "mafiagame-e348d.firebasestorage.app",
-  messagingSenderId: "915924828018",
-  appId: "1:915924828018:web:190bfb4241a96dc197e7eb"
+  apiKey: "AIzaSyAPlcENvc11dXeo-LCwTiDkOUvQlV1--sY",
+  authDomain: "mafia2game.firebaseapp.com",
+  projectId: "mafia2game",
+  storageBucket: "mafia2game.firebasestorage.app",
+  messagingSenderId: "623190816943",
+  appId: "1:623190816943:web:3830b8f4928ae5a7527c5a"
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
